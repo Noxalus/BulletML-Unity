8a425ada8ccf5ccd031c07f72d269aa17058f114
Noxalus <noxalus@gmail.com>
2018-11-06 23:33:15 +0100

    Update the BulletRenderer to render the bullet on a specific layer and camera.

--- BulletML-Unity/Assets/Runtime/Scripts/Bullets/BulletRenderer.cs
+++ BulletML-Unity/Assets/Runtime/Scripts/Bullets/BulletRenderer.cs
@@ -7,6 +7,8 @@ namespace UnityBulletML.Bullets
         [SerializeField] private Material _material = null;
         [SerializeField] private BulletManager _bulletManager = null;
         [SerializeField] private float _bulletSize = 1f;
+        [SerializeField] private int _layer = 0;
+        [SerializeField] private Camera _camera= null;
 
         private Mesh _mesh;
         MaterialPropertyBlock _materialPropertyBlock;
@@ -39,7 +41,10 @@ namespace UnityBulletML.Bullets
                 _materialPropertyBlock.SetVectorArray("_MainTex_ST", _bulletManager.BulletSpriteOffsetsBatches[i]);
                 _materialPropertyBlock.SetVectorArray("_Color", _bulletManager.BulletColorsBatches[i]);
 
-                Graphics.DrawMeshInstanced(_mesh, 0, _material, transformMatrixBatch, transformMatrixBatch.Length, _materialPropertyBlock);
+                Graphics.DrawMeshInstanced(
+                    _mesh, 0, _material, transformMatrixBatch, transformMatrixBatch.Length, _materialPropertyBlock,
+                    UnityEngine.Rendering.ShadowCastingMode.Off, false, _layer, _camera
+                );
             }
         }
     }
