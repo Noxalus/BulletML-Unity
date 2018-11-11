5510be8f198cb0ddea4f02252026d77ddef21af1
Noxalus <noxalus@gmail.com>
2018-11-11 18:44:03 +0100

    Fix an issue where the bullet intial size was not properly taken into account in the BulletPhysics script to compute collisions.

--- BulletML-Unity/Assets/Runtime/Scripts/Bullets/BulletPhysics.cs
+++ BulletML-Unity/Assets/Runtime/Scripts/Bullets/BulletPhysics.cs
@@ -44,7 +44,7 @@ namespace UnityBulletML.Bullets
 
                 var dx = (currentBullet.Position.x / _bulletManager.PixelPerUnit) - _playerTransform.position.x;
                 var dy = (currentBullet.Position.y / _bulletManager.PixelPerUnit) - _playerTransform.position.y;
-                var radius = currentBullet.GetProfile().CollisionRadius + _playerCollider.radius;
+                var radius = (currentBullet.GetProfile().CollisionRadius * _bulletManager.BulletInitialSize) + _playerCollider.radius;
 
                 if ((dx * dx) + (dy * dy) < radius * radius)
                 {
